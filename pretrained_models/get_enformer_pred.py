83c11665ffcf74146bc7b663c404bd1ababd90f0
maple <maplecai142857@gmail.com>
2024-12-04 03:09:31 -0500

    merge

--- pretrained_models/get_enformer_pred.py
+++ pretrained_models/get_enformer_pred.py
@@ -10,6 +10,7 @@ from enformer_pytorch import Enformer, from_pretrained
 from MPRA_predict.utils import *
 from MPRA_predict.datasets import SeqLabelDataset
 
+
 # def get_pred(model, test_data_loader, device):
 #     model = model.to(device)
 #     y_pred = []
@@ -79,34 +80,3 @@ dataset = SeqLabelDataset(data_path='/home/hxcai/cell_type_specific_CRE/data/Sir
                           seq_column='seq', padding=True, padded_len=256, padding_mode='random', N_fill_value=0)
 test_data_loader = DataLoader(dataset, batch_size=1024, shuffle=False, num_workers=0)
 y_pred = get_pred(model, test_data_loader, device, save_interval=10, save_prefix='data/Enformer_Siraj_pred_part')
-
-
-
-
-# def get_embedding(model, test_data_loader, device='cuda'):
-#     model = model.to(device)
-#     y_true = []
-#     y_pred = []
-#     embedding = []
-#     with torch.no_grad():
-#         model.eval()
-#         for (x, y) in tqdm(test_data_loader):
-#             x = x.to(device)
-#             x_rc = onehots_reverse_complement(x).to(device)
-#             pred_1, emb_1 = model(x, return_embeddings = True)
-#             pred_2, emb_2 = model(x_rc, return_embeddings = True)
-#             pred = (pred_1['human'] + pred_2['human']) / 2
-#             emb  = (emb_1 + emb_2) / 2
-#             # x = x.to(device)
-#             # pred, emb = model(x, return_embeddings = True)
-#             # pred = pred['human']
-#             # print(y.shape, pred.shape, emb.shape)
-
-#             y_true.extend(y.cpu().detach().numpy())
-#             y_pred.extend(pred.cpu().detach().numpy())
-#             embedding.extend(emb.cpu().detach().numpy())
-
-#     y_true = np.array(y_true)
-#     y_pred = np.array(y_pred)
-#     embedding = np.array(embedding)
-#     return y_true, y_pred, embedding
